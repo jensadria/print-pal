436e20443bdabb8457f81b0c390b40e159adfdd7
Jens Adria <jensadria@gmail.com>
2020-12-27 14:32:13 +0800

    Canvas styling fixes and first imposition functionality

--- src/components/nup/NupCanvas.vue
+++ src/components/nup/NupCanvas.vue
@@ -5,6 +5,7 @@
       :width="nUpInput.sheetWidth"
       :height="nUpInput.sheetHeight"
     ></canvas>
+    <p><button type="button" @click="drawRect()">Test</button></p>
   </div>
 </template>
 
@@ -22,25 +23,55 @@ export default {
     nUpResult() {
       return this.$store.getters.loadResults;
     },
-    result() {
-      return this.mostOut ? this.nUpResult.nUp1 : this.nUpResult.nUp2;
-    },
+    // result() {
+    //   return this.mostOut ? this.nUpResult.nUp1 : this.nUpResult.nUp2;
+    // },
   },
   mounted() {
     const c = document.getElementById('nup-canvas');
     const ctx = c.getContext('2d');
     this.vueCanvas = ctx;
   },
+  methods: {
+    drawRect() {
+      let width = this.nUpInput.width;
+      let height = this.nUpInput.height;
+      let startingPointX = this.nUpInput.margins;
+      let startingPointY = this.nUpInput.margins;
+      let margin = this.nUpInput.margins;
+      // let qty = this.nUpInput.qty;
+
+      this.vueCanvas.clearRect(
+        0,
+        0,
+        this.nUpInput.sheetWidth,
+        this.nUpInput.sheetHeight
+      );
+      // this.vueCanvas.beginPath();
+      this.vueCanvas.fillRect(startingPointX, startingPointY, width, height);
+      startingPointX = startingPointX + width + margin;
+      this.vueCanvas.fillRect(startingPointX, startingPointY, width, height);
+      startingPointX = startingPointX + width + margin;
+      this.vueCanvas.fillRect(startingPointX, startingPointY, width, height);
+      startingPointX = startingPointX + width + margin;
+      this.vueCanvas.fillRect(startingPointX, startingPointY, width, height);
+      // this.vueCanvas.stroke();
+    },
+  },
 };
 </script>
 
 <style scoped>
-/* .canvas-container {
+.canvas-container {
   width: 100%;
   height: 100%;
-} */
+  display: block;
+}
 
 #nup-canvas {
-  border: 1px solid black;
+  border: 1px solid red;
+  position: relative;
+  width: 100%;
+  height: auto;
 }
 </style>

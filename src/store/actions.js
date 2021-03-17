f63fe3bf882b4aac3bf48cb0c3e8c62b2052869a
jensadria <jensadria@gmail.com>
2021-03-17 19:41:34 +0800

    Secure server again

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,31 +3,31 @@ const axios = require('axios');
 export default {
   async LOAD_PRODUCTS(context) {
     await axios
-      .get('http://print-pal.herokuapp.com/api/products/')
+      .get('https://print-pal.herokuapp.com/api/products/')
       .then((response) => context.commit('LOAD_PRODUCTS', response.data))
       .catch((error) => console.error(error));
   },
   async LOAD_ORDERS(context) {
     await axios
-      .get('http://print-pal.herokuapp.com/api/orders/')
+      .get('https://print-pal.herokuapp.com/api/orders/')
       .then((response) => context.commit('LOAD_ORDERS', response.data))
       .catch((error) => console.error(error));
   },
   async addOrderToDb(_, payload) {
     await axios
-      .post('http://print-pal.herokuapp.com/api/orders/', payload)
+      .post('https://print-pal.herokuapp.com/api/orders/', payload)
       .then((response) => console.log(response.data))
       .catch((error) => console.error(error));
   },
   async editOrder(_, { orderId, savedOrder }) {
     await axios
-      .put(`http://print-pal.herokuapp.com/api/orders/${orderId}`, savedOrder)
+      .put(`https://print-pal.herokuapp.com/api/orders/${orderId}`, savedOrder)
       .then((response) => console.log(response.data))
       .catch((error) => console.error(error));
   },
   async deleteOrder(_, orderId) {
     await axios
-      .delete(`http://print-pal.herokuapp.com/api/orders/${orderId}`)
+      .delete(`https://print-pal.herokuapp.com/api/orders/${orderId}`)
       .then((response) => console.log(response.data))
       .catch((error) => console.error(error));
   },

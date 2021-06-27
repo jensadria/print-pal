684c2c6701159b95bfea72a99b645da42fa78a6b
Jens <jensadria@gmail.com>
2021-06-27 23:05:13 +0800

    Added extra validation for inputs too big, added footer and contact email, max height on svg

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div class="container">
     <the-header></the-header>
     <router-view></router-view>
+    <the-footer></the-footer>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/layout/TheHeader.vue';
+import TheFooter from './components/layout/TheFooter.vue';
 
 export default {
   name: 'App',
   components: {
     TheHeader,
+    TheFooter,
   },
 };
 </script>
